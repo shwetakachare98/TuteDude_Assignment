cf449e9eae2f90a4d2a25112dee2863954387a2c
shwetakachare98 <shweta.kachare04@outlook.com>
2025-07-28 14:59:23 +0530

    Update Task_1.py

--- Assignment_5/Task_1.py
+++ Assignment_5/Task_1.py
@@ -1,9 +1,7 @@
-student_marks={'sanika':'45','jiya':'67','priya':'55','Alice':'85'}
-student_name=input("Enter your name : ")
+student_marks={'Siya':'45','Jiya':'67','Priya':'55','Alice':'85'}
+student_name=input("Enter your student's name : ")
 
 if student_name in student_marks:
-
    print(student_name+"'s Marks:",student_marks[student_name])
-
 else:
    print("Student not found")
